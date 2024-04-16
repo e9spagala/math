f03f53c657db0047a840591b1414a589ef81c445
Charlie <sri.pagala@enine.school>
2024-04-16 16:41:53 +0530

    returned sum instead of list

--- math002/solution.py
+++ math002/solution.py
@@ -4,21 +4,22 @@ from typing import List, Union
 LIMIT = 4_000_000
 def answer() -> None:
     
-    even_fibonacci: List[int] = []
+    
     
     f2: int = 1
     f3: int = 2
+    total: int = 0
     while f2 <= LIMIT:
         if f2 & 1 == 0:
-            even_fibonacci.append(f2)
+            total += f2
         f2, f3 = f3, f2 + f3
-    print(even_fibonacci)
+    print(total)
 
 answer()
 
 #PART B
 
-def solver(start, end, even=False, odd=False) -> Union[None, List[int]]:
+def solver(start, end, even=False, odd=False) -> Union[None, int]:
 
     if start > end:
         return None
@@ -26,14 +27,14 @@ def solver(start, end, even=False, odd=False) -> Union[None, List[int]]:
     if not even and not odd: 
         return []
 
-    even_odd_fibonacci: List[int] = []
+    total: int = 0
 
     if even:
         f2: int = 1
         f3: int = 2
         while f2 <= end:
             if f2 >= start and f2 & 1 == 0:
-                even_odd_fibonacci.append(f2)
+                total +=f2
             f2, f3 = f3, f2 + f3
 
     if odd:
@@ -41,9 +42,9 @@ def solver(start, end, even=False, odd=False) -> Union[None, List[int]]:
         f3: int = 2
         while f2 <= end:
             if f2 >= start and f2 & 1 != 0:
-                even_odd_fibonacci.append(f2)
+                total +=f2
             f2, f3 = f3, f2 + f3
-    return even_odd_fibonacci 
+    return total
 
 print(solver(2_000_000, 4_000_000, even=True, odd=False))
 
