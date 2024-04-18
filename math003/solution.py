26c2319fb00d8b274e6f7e048dd9582fc1a8713b
Charlie <sri.pagala@enine.school>
2024-04-18 12:20:10 +0530

    updated the solution

--- math003/solution.py
+++ math003/solution.py
@@ -1,25 +1,18 @@
 from typing import List, Dict
-
 def isPrime(n: int) -> bool:
     i: int = 2
-    count: int = 0
     while i * i <= n:
         if n % i == 0:
-            count += 1
-        #  mitigating duplicate factors
-        if n / i != i:
-            count += 1
-        if count > 2:
             return False
-    
-    return False if count > 2 else True
+        i += 1
+    return True
 
 LIMIT: int = 10_000_000 # assuming 10^7 is the max limit for the given input
 primes: List[bool] = [True for _ in range(LIMIT + 1)]
 primes[0] = False
 primes[1] = False
 
-# Sieve of Eratosthenes
+# # Sieve of Eratosthenes
 for i in range(2, LIMIT + 1):
     if primes[i] == False:
         continue
@@ -30,36 +23,23 @@ for i in range(2, LIMIT + 1):
 
 # Part A
 
-inp: List[int] = [600,851,475,143]
+inp: int = 600_851_475_143
 
-def answer() -> Dict[int, int]:
-    max_primes: Dict[int, int] = {}
-    for ele in inp:
-        max_prime = 2
-        i = 2
-        while i * i <= ele:
-            if ele % i == 0:
-                if primes[i] == True:
-                    max_prime = max(max_prime, i)
-                # if this is not a duplicate factor then check for this too
-                other_factor = ele // i
-                if other_factor // i != i:
-                    if primes[other_factor] == True:
-                        max_prime = max(max_prime, other_factor)
-                
-            i += 1
-        max_primes[ele] = max_prime
+def answer() -> int:
+    max_prime :int = 2
+    i :int = 2
+    while i* i <= inp:
+        if inp % i == 0:
+            if isPrime(i):
+                max_prime = max(max_prime, i)
+            if isPrime(inp // i):
+                max_prime = max(max_prime, inp // i)
+        i += 1
+    return max_prime
+ans: int = answer()
 
-    return max_primes
-
-ans: Dict[int, int] = answer()
-
-for key, val in ans.items():
-    print(f'Maximum prime factor of {key}: {val}')
-
-
+print(ans)
 # Part B
-
 
 def solver(n: int) -> int:
     max_prime = 2
