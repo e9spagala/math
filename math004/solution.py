e86d4855f005b9daf39849d06461648182b366e9
Charlie <sri.pagala@enine.school>
2024-04-18 17:40:50 +0530

    largest palindrome that is product of 2-3dig nums

---
+++ math004/solution.py
@@ -0,0 +1,37 @@
+from typing import Union
+def is_palindrome(n: int) -> bool:
+    num:str = str(n)
+    return num == num[::-1]
+# Part A
+
+## naive approah
+
+def answer() -> int:
+    last_palindrome: int = -1
+    for i in range(100, 1000):
+        for j in range(100, 1000):
+            prod = i * j
+            if is_palindrome(prod):
+                last_palindrome = max(last_palindrome, prod)
+                print(last_palindrome)
+    return last_palindrome
+
+print(answer())
+
+# Part B
+
+def solver(n: int, p: int, q: int) -> Union[int, None]:
+    
+    low: int = 10**(n-1)
+    high: int = low* 10
+
+    last_palindrome = -1
+    for i in range(low, high):
+        for j in range(low, high):
+            prod = i * j
+            if prod >= p and prod <= q and is_palindrome(prod):
+                last_palindrome = max(last_palindrome, prod)
+
+    return last_palindrome
+
+print(solver(3, 1000, 1000000 ))
