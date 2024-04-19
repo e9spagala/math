c589f4a9131257ab0605ab2b1f2c37204bf6acc3
Charlie <sri.pagala@enine.school>
2024-04-19 18:38:21 +0530

    sevie

--- math07/solution.py
+++ math07/solution.py
@@ -1,24 +1,21 @@
-def is_prime(n: int) -> bool:
-    if n == 1:
-        return False
-    i = 2
 
-    while i * i <= n:
-        if n % i == 0:
-            return False
-        if n % (n // i) == 0:
-            return False
-        i += 1
-    return True 
-
-
-maxi = 10_001
+maxi = 1_000_000
 LIMIT = maxi * maxi
+
+prime = [True for _ in range(LIMIT + 1)]
+
+prime[0] = False
+prime[1] = False
+
+for i in range(2, LIMIT + 1):
+    if prime[i] == True:
+        for j in range(i * i, LIMIT + 1, i ):
+            prime[j] = False
 
 def answer() -> int:
     temp = maxi
     for i in range(2, LIMIT + 1):
-        if is_prime(i) == True:
+        if prime[i] == True:
             temp -= 1
         if temp == 0:
             return i
@@ -31,7 +28,7 @@ def solver(n: int) -> int:
     LIMIT = n * n
     temp = maxi
     for i in range(2, LIMIT + 1):
-        if is_prime(i) == True:
+        if prime[i] == True:
             temp -= 1
         if temp == 0:
             return i
