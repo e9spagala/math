0f24cbf9f08611d70f48c237559b356f05610995
theunhackable <srirangasaipagala@gmail.com>
2024-04-20 16:24:26 +0530

    added seive logic

--- math07/solution.py
+++ math07/solution.py
@@ -1,32 +1,33 @@
+# How do you know the ith prime lies in a certain range in order to find the seive length.
 
-maxi = 1_000_000
-LIMIT = maxi * maxi
+maxi = 15_485_863
+
+LIMIT = maxi 
 
 prime = [True for _ in range(LIMIT + 1)]
 
 prime[0] = False
 prime[1] = False
 
-for i in range(2, LIMIT + 1):
-    if prime[i] == True:
-        for j in range(i * i, LIMIT + 1, i ):
-            prime[j] = False
+
 
 def answer() -> int:
-    temp = maxi
+    t = 1_000_000
     for i in range(2, LIMIT + 1):
         if prime[i] == True:
-            temp -= 1
-        if temp == 0:
-            return i
-        
+            t -= 1
+            if t == 0:
+                return i
+            for j in range(i * i, LIMIT + 1, i ):
+                prime[j] = False
+            
 print(answer())
 
 
 
 def solver(n: int) -> int:
-    LIMIT = n * n
-    temp = maxi
+    
+    temp = n
     for i in range(2, LIMIT + 1):
         if prime[i] == True:
             temp -= 1
